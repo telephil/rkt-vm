9d3538005d29d665e34a10c75e6d569d5287c727
Philippe Mechai <philippe.mechai@gmail.com>
2012-09-24 23:00:26 +0200

    parser.rkt : fixed parsing using src-pos

--- parser.rkt
+++ parser.rkt
@@ -3,8 +3,12 @@
 (require "insn.rkt"
          parser-tools/yacc
          parser-tools/lex
-         syntax/readerr
          (prefix-in : parser-tools/lex-sre))
+
+(provide/contract
+ [parse (-> input-port? string? (listof (or/c label? insn?)))]
+ [parse-string (-> string? (listof (or/c label? insn?)))]
+ [parse-file (-> string? (listof (or/c label? insn?)))])
 
 ;; Utils
 (define (bin-string->number str)
@@ -18,14 +22,15 @@
 
 ;; Lexer
 (define asm-lexer
-  (lexer ;-src-pos
+  (lexer-src-pos
+   ;; EOF
    [(eof) 'EOF]
    
    ;; Skip whitespaces
-   [(:or #\tab #\space #\newline) (asm-lexer input-port)]
+   [(:or #\tab #\space #\newline) (return-without-pos (asm-lexer input-port))]
    
    ;; Comma
-   ["," (token-COMMA)]
+   [#\, 'COMMA]
 
    ;; Register
    [(:or (:: (:or #\r #\R) (:/ "0" "8"))
@@ -45,33 +50,30 @@
    [(:: (:+ (:or "0" "1")) "b")
     (token-NUMBER (bin-string->number lexeme))]
    ;;; - Decimal
-   [(:+ numeric) (token-NUMBER (string->number lexeme))]
+   [(:+ numeric)
+    (token-NUMBER (string->number lexeme))]
    ;;; - Hexadecimal
    [(:: "0x" (:+ (:or (:/ "0" "9") (:/ "a" "f") (:/ "A" "F"))))
     (token-NUMBER (string->number (substring lexeme 2) 16))]))
 
 ;; Parser
-(define asm-parser
+(define (asm-parser source-name)
   (parser
-   ;(src-pos)
+   (src-pos)
    
    (start s)
    (end   EOF)
    (tokens data delim)
   
-   (error (lambda (a b c)
-            (printf "ERROR: ~a - ~a~%" b c)))
-   #|
    (error (lambda (a name val start end)
-            (raise-read-error 
-             "read-error"
-             ""
+            (raise-user-error
+             (format 
+              "~a: parse error at line ~a column ~a : unexpected token ~a."
+             source-name
              (position-line start)
              (position-col start)
-             (position-offset start)
-             (- (position-offset end)
-                (position-offset start)))))
-|#
+             val))))
+
    (grammar
     (s
      [(insn-list) (reverse $1)])
@@ -86,14 +88,23 @@
      [() null]
      [(insn-list insn) (cons $2 $1)]))))
 
-(define (parse ip)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public parser API
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Parse from an input port
+(define (parse name ip)
   (port-count-lines! ip)
-  (asm-parser
+  ((asm-parser name)
    (lambda ()
      (asm-lexer ip))))
 
+;; Parse given string
 (define (parse-string str)
-  (call-with-input-string str parse))
+  (call-with-input-string str                          
+    (lambda (ip) (parse "<unknown>" ip))))
 
+;; Parse from file
 (define (parse-file filename)
-  (call-with-input-file filename parse))
+  (call-with-input-file filename 
+    (lambda (ip) (parse filename ip))))
