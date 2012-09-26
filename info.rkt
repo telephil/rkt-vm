de66a78f7067e937a981b8882bb11ae0e5af983e
Philippe Mechai <philippe.mechai@gmail.com>
2012-09-26 19:48:56 +0200

    Changed info.rkt to remove use of setup/infotab (couldn't get it to work :/)

--- info.rkt
+++ info.rkt
@@ -1,8 +1,14 @@
-#lang setup/infotab
+#lang racket
 
-(define name "VM")
-(define blurb
-  (list "A poorly implemented virtual machine"))
-(define categories '(devtools))
-;(define primary-file "racket.rkt")
-(define version "0.0.1")
+(provide vm-author vm-version)
+;; #lang setup/infotab
+
+;; (define name "VM")
+;; (define blurb
+;;   (list "A poorly implemented virtual machine"))
+;; (define categories '(devtools))
+;; ;(define primary-file "racket.rkt")
+;; (define version "0.0.1")
+
+(define vm-author "Philippe Mechaï")
+(define vm-version "0.0.1")
