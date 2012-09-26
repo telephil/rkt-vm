1e44ec2180dc79bdd9725320ab134ef381e7e920
Philippe Mechai <philippe.mechai@gmail.com>
2012-09-26 19:37:02 +0200

    Added an info.rkt file which contains package information

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang setup/infotab
+
+(define name "VM")
+(define blurb
+  (list "A poorly implemented virtual machine"))
+(define categories '(devtools))
+;(define primary-file "racket.rkt")
+(define version "0.0.1")
