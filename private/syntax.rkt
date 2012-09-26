6e4a179312adb2e884093c2e8cd4e49fb70b8830
Philippe Mechai <philippe.mechai@gmail.com>
2012-09-26 22:35:35 +0200

    syntax.rkt: added inspectors to syntax structs

--- private/syntax.rkt
+++ private/syntax.rkt
@@ -9,8 +9,17 @@
  (struct-out register-stx)
  (struct-out number-stx))
 
-(struct label-stx (name))
-(struct opcode-stx (name))
-(struct register-stx (name))
-(struct number-stx (value))
-(struct insn-stx (op arg1 arg2))
+(struct label-stx (name)
+  #:inspector (make-inspector))
+
+(struct opcode-stx (name)
+  #:inspector (make-inspector))
+
+(struct register-stx (name)
+  #:inspector (make-inspector))
+
+(struct number-stx (value)
+  #:inspector (make-inspector))
+
+(struct insn-stx (op arg1 arg2)
+  #:inspector (make-inspector))
