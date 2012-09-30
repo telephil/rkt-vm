b88ba5401fd8d7a2efe1406d329f5ac57a3419a8
Philippe Mechai <philippe.mechai@gmail.com>
2012-09-30 17:21:08 +0200

    Rewrite of memory.rkt to use bytes? instead of bytevector?

--- private/memory.rkt
+++ private/memory.rkt
@@ -1,37 +1,58 @@
 #lang racket
 
-(require rnrs/bytevectors-6)
+;(require rnrs/bytevectors-6)
 
 (define (index? x)
   (and (integer? x) (or (zero? x) (positive? x))))
 
 (provide/contract
  [create-memory (-> (and integer? positive?) void)]
- [store-qword   (-> bytevector? index? integer? void)]
- [load-qword    (-> bytevector? index? integer?)]
- [store-dword   (-> bytevector? index? integer? void)]
- [load-dword    (-> bytevector? index? integer?)]
- [store-word    (-> bytevector? index? integer? void)]
- [load-word     (-> bytevector? index? integer?)]
- [store-byte    (-> bytevector? index? integer? void)]
- [load-byte     (-> bytevector? index? integer?)])
+ [store-qword   (-> bytes? index? integer? void)]
+ [load-qword    (-> bytes? index? integer?)]
+ [store-dword   (-> bytes? index? integer? void)]
+ [load-dword    (-> bytes? index? integer?)]
+ [store-word    (-> bytes? index? integer? void)]
+ [load-word     (-> bytes? index? integer?)]
+ [store-byte    (-> bytes? index? integer? void)]
+ [load-byte     (-> bytes? index? integer?)])
 
 ;; create-memory
 (define (create-memory size)
-  (make-bytevector size 0))
+  (make-bytes size 0))
+
+(define (store dest idx v sz)
+  (define data (integer->integer-bytes v sz #t))
+  (bytes-copy! dest idx data))
+
+(define (load dest idx sz)
+  (define data (subbytes dest idx (+ idx sz)))
+  (integer-bytes->integer data #t))
 
 ;; qword
-(define store-qword bytevector-u64-native-set!)
-(define load-qword bytevector-u64-native-ref)
+(define (store-qword dest idx v)
+  (store dest idx v 8))
+
+(define (load-qword dest idx)
+  (load dest idx 8))
 
 ;; dword
-(define store-dword bytevector-u32-native-set!)
-(define load-dword bytevector-u32-native-ref)
+(define (store-dword dest idx v)
+  (store dest idx v 4))
+
+(define (load-dword dest idx)
+  (load dest idx 4))
 
 ;; word
-(define store-word bytevector-u16-native-set!)
-(define load-word bytevector-u16-native-ref)
+(define (store-word dest idx v)
+  (store dest idx v 2))
+
+(define (load-word dest idx)
+  (load dest idx 2))
 
 ;; byte
-(define store-byte bytevector-u8-set!)
-(define load-byte bytevector-u8-ref)
+(define (store-byte dest idx v)
+  (bytes-set! dest idx v))
+
+(define (load-byte dest idx)
+  (bytes-ref dest idx))
+
