46816b5ff9cb3ba6211db2d2e08e25ffc4b63e85
Philippe Mechai <philippe.mechai@gmail.com>
2012-10-22 00:02:27 +0200

    Add missing "stack (de)allocation" calls

--- samples/factorial_localarg.asm
+++ samples/factorial_localarg.asm
@@ -1,10 +1,12 @@
 fact:   push    bp
         mov     bp,     sp
+        sub     sp,     8
         mov     -8(bp), 1
 loop:   mul     -8(bp), 16(bp)
         dec     16(bp)
         jnz     loop
         mov     r0,     -8(bp)
+        add     sp,     8
         pop     bp
         ret
 start:  push    5
