7516e9986ed4d7084947816ad64b30e1e5d8f8c9
Philippe Mechai <philippe.mechai@gmail.com>
2012-10-22 23:33:34 +0200

    Added comments

--- samples/factorial_func_write.asm
+++ samples/factorial_func_write.asm
@@ -1,3 +1,5 @@
+;;; Sample use of the syscall op
+;;; once computed the result of calling fact is printed
 fact:   mul     r0,     r1
         dec     r1
         jnz     fact
@@ -5,9 +7,11 @@ fact:   mul     r0,     r1
 start:  mov     r0,     1
         mov     r1,     5
         call    fact
+        ;; syscall 10: write-int
         mov     r1,     r0
         mov     r0,     10
         syscall
+        ;; syscall 11: write-char
         mov     r1,     10
         mov     r0,     11
         syscall
